63e99db3e6cd3cc4476dd453b336e3dcb959b26b
joetheory <joetheory@gmail.com>
2023-01-24 00:28:03 -0500

    oh my

--- Scripts/ClassSelection.gd
+++ Scripts/ClassSelection.gd
@@ -14,4 +14,9 @@ func _ready() -> void:
 	for character_class in character_classes:
 		var button = Button.new()
 		button.text = character_class.name
+		button.pressed.connect(setCharacterClass.bind(character_class))
 		$CenterContainer/VBoxContainer.add_child(button)
+		
+		
+func setCharacterClass(character_class : CharacterClass) -> void:
+	PlayerManager.character_class = character_class
